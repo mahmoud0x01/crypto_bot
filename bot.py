e0e5b797cc58753723f6eba4f1da158e82db76a1
Mahmoud Adel <mahmoudad707@gmail.com>
2025-01-05 16:37:10 +0700

    Task #25777 Telegram Bot init

--- bot.py
+++ bot.py
@@ -25,3 +25,23 @@ def send_telegram_message(message):
         print("telegram Message sent successfully")
     else:
         print("Failed to send message:", response.text)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the /start command is issued, if from allowed chat ID."""
+    if str(update.message.chat_id) == str(chat_id):
+        await update.message.reply_text("Hello! You're authorized to use this bot.")
+        print("someone clicked start")
+    else:
+        await update.message.reply_text("You're not authorized to use this bot.")
+
+
+def run_bot() -> None:
+    """Start the bot and listen for commands."""
+    # Create the Application and pass the bot's token
+    application = Application.builder().token(bot_token).build()
+    application.add_handler(CommandHandler("start", start))
+
+
+
+run_bot()
